441ba2e8461675e3798f0ae81d96cee2e4c8ba69
Markus Kahl <machisuji@gmail.com>
2016-11-19 15:39:20 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# cardboard
+
+[![travis ci](https://travis-ci.org/machisuji/cardboard.svg?branch=master)](https://travis-ci.org/machisuji/cardboard)
+
+`cardboard` is a decentralized, kanban-style collaboration tool which lets you organize tasks
+into customizable cards based on plain, versioned text files.
+
+## Build
+
+Built using cargo.
+
+```
+cargo build --release
+```
+
+Whereupon the executable will be created at `target/release/cardboard`.
---
+++ src/main.rs
@@ -0,0 +1,97 @@
+extern crate iron;
+extern crate hoedown;
+extern crate hyper;
+extern crate webbrowser;
+extern crate router;
+extern crate staticfile;
+
+use iron::prelude::*;
+use iron::status;
+
+use hoedown::{Markdown, Render};
+use hoedown::renderer::html::{Flags, Html};
+
+use hyper::header::{ContentType};
+use hyper::mime::{Mime, TopLevel, SubLevel};
+
+use std::io::prelude::*;
+use std::path::Path;
+use std::fs::File;
+use std::fs;
+
+use router::Router;
+use staticfile::Static;
+
+fn main() {
+    fn make_router() -> Router {
+        let mut router = Router::new();
+
+        router.get("/", index, "index");
+        router.get("*", file_handler(), "files");
+
+        router
+    }
+
+    fn index(_: &mut Request) -> IronResult<Response> {
+        let mut html = String::with_capacity(4096);
+
+        render_response(String::from("Karten - Index"), &mut html);
+
+        html_response(html)
+    }
+
+    fn file_handler() -> Static {
+        Static::new(Path::new("public/"))
+    }
+
+    fn render_response(title: String, output: &mut String) {
+        output.push_str("<html><head><title>");
+        output.push_str(& title);
+        output.push_str("</title>");
+
+        output.push_str("<link rel=\"stylesheet\" type=\"text/css\" href=\"main.css\">");
+        output.push_str("</head><body>");
+
+        render_cards(output);
+
+        output.push_str("</body>");
+    }
+
+    fn render_cards(output: &mut String) {
+        for card in fs::read_dir(".karten/cards").ok().unwrap().flat_map(|dir| dir.map(|e| e.path())) {
+            let mut file = File::open(& card).ok().unwrap();
+            let mut source = String::new();
+
+            output.push_str("<div class=\"card\">");
+
+            if file.read_to_string(&mut source).is_ok() {
+                let doc = Markdown::new(&source);
+                let mut html = Html::new(Flags::empty(), 0);
+                let result = html.render(&doc);
+
+                output.push_str(result.to_str().unwrap());
+            } else {
+                output.push_str("Could not read: ");
+                output.push_str(card.to_str().unwrap());
+            }
+
+            output.push_str("</div>");
+        }
+    }
+
+    fn html_response(content: String) -> IronResult<Response> {
+        let mut response = Response::with((status::Ok, content));
+
+        response.headers.set(ContentType(Mime(TopLevel::Text, SubLevel::Html, vec![])));
+
+        Ok(response)
+    }
+
+    println!("Listening on port 9000 ...");
+
+    if !webbrowser::open("http://localhost:9000/").is_ok() {
+        println!("Open http://localhost:9000 to view your project")
+    }
+
+    Iron::new(make_router()).http("localhost:9000").unwrap();
+}
